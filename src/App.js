cf40bef6aa0293b1f8d97d206be18e26f32f76a7
VysakhAlrajhi <vysakh.accordinnovations@alrajhibank.com.my>
2025-09-23 17:16:53 +0800

    Refactor App component for improved readability and update link text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Code World!</p>
+        <p>
+         Welcome to the Code World!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          CI/CD with React
+          Learn React
         </a>
       </header>
     </div>
